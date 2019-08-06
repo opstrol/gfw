6ea9046c0f37d2ccd8958627cf2dde71285d8c8d
seebuf <1287188193@qq.com>
2019-08-06 16:26:10 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
 #	Description: Install the ShadowsocksR server
@@ -9,7 +8,6 @@ export PATH
 #	Author: Toyo
 #	Blog: https://doub.io/ss-jc42/
 #=================================================
-
 sh_ver="2.0.38"
 filepath=$(cd "$(dirname "$0")"; pwd)
 file=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
